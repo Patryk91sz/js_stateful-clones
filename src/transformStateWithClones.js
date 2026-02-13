ce65062fe7cbfbd918570b00fe241840bc2d7013
Saper <patryk.szyperek@gmail.com>
2026-02-13 19:21:35 +0100

    Multiple if to switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,6 +6,7 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
   // write code here
   const result = [];
@@ -14,22 +15,22 @@ function transformStateWithClones(state, actions) {
   for (const action of actions) {
     const newState = { ...currentState };
 
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        newState[key] = action.extraData[key];
-      }
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        newState[key] = action.keysToRemove[key];
-      }
-    }
-
-    if (action.type === 'clear') {
-      for (const key in newState) {
-        delete newState[key];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          newState[key] = action.extraData[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        break;
     }
 
     result.push(newState);
@@ -38,5 +39,32 @@ function transformStateWithClones(state, actions) {
 
   return result;
 }
-
 module.exports = transformStateWithClones;
+
+//   for (const action of actions) {
+//     const newState = { ...currentState };
+//
+//     if (action.type === 'addProperties') {
+//       for (const key in action.extraData) {
+//         newState[key] = action.extraData[key];
+//       }
+//     }
+//
+//     if (action.type === 'removeProperties') {
+//       for (const key of action.keysToRemove) {
+//         newState[key] = action.keysToRemove[key];
+//       }
+//     }
+//
+//     if (action.type === 'clear') {
+//       for (const key in newState) {
+//         delete newState[key];
+//       }
+//     }
+//
+//     result.push(newState);
+//     currentState = newState;
+//   }
+//
+//   return result;
+//
